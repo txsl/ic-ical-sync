d6658b8eb4666a937c7d8113ea5e3646c7937479
Thomas Lim <txsl@Thomass-MacBook-Pro.local>
2014-01-27 21:28:39 +0000

    Initial commit of barebones project

---
+++ process.py
@@ -0,0 +1,32 @@
+from pyexchange import Exchange2010Service, ExchangeNTLMAuthConnection
+from datetime import datetime
+from pytz import timezone
+import getpass
+
+# This modification: https://code.google.com/p/python-ntlm/issues/detail?id=17 needs to be made to NTLM, otherwise this code fails.
+
+URL = u'https://exchange.imperial.ac.uk/EWS/Exchange.asmx'
+
+try:
+	from credentials import *
+except ImportError:
+	print "Credentials file not found. Please enter your details below..."
+	USERNAME = u'ic\\' + raw_input("Imperial College Username: ")
+	PASSWORD = getpass.getpass("Imperial College Password:")
+
+# Set up the connection to Exchange
+connection = ExchangeNTLMAuthConnection(url=URL,
+                                     username=USERNAME,
+                                     password=PASSWORD)
+
+service = Exchange2010Service(connection)
+
+event= service.calendar().new_event(
+	subject= u"This is a test",
+	location= u"EEE",
+	start=datetime(2014,2,1,15,0,0, tzinfo=timezone("Europe/London")),
+	end=datetime(2014,2,1,16,0,0, tzinfo=timezone("Europe/London")),
+	text_body= u"does this appear"
+)
+
+event.create()
